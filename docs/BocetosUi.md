80e7f7b1a1e2d93aa022418e5a10a56ffb2dbe62
marianoapn <marianoapn@gmail.com>
2024-11-10 21:30:06 -0300

    Modificaciones EsLint

--- docs/BocetosUi.md
+++ docs/BocetosUi.md
@@ -1,8 +1,7 @@
 # Bocetos de la UI de Botija
 
-
 <img src="ImagenesInterfaz/Splash.png" alt="Botija Splash"> 
 <img src="ImagenesInterfaz/Home.png" alt="Botija Home Page" > 
 <img src="ImagenesInterfaz/Login.png" alt="Botija Login Page"> 
 <img src="ImagenesInterfaz/Menú lateral.png" alt="Botija Menu Lateral"> 
-<img src="ImagenesInterfaz/Registro.png" alt="Botija Registro"> 
+<img src="ImagenesInterfaz/Registro.png" alt="Botija Registro">
--- entrega2/js/lesson.js
+++ entrega2/js/lesson.js
@@ -1,25 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 class Lesson {
-    constructor(id, date, topic, description, curriculumUnit) {
-        this.id = id;
-        this.date = date;
-        this.topic = topic;
-        this.description = description;
-        this.curriculumUnit = curriculumUnit;
-    }
+  constructor(id, date, topic, description, curriculumUnit) {
+    this.id = id;
+    this.date = date;
+    this.topic = topic;
+    this.description = description;
+    this.curriculumUnit = curriculumUnit;
+  }
 
-    // Método para editar una lección
-    editLesson(topic, description, curriculumUnit) {
-        this.topic = topic;
-        this.description = description;
-        this.curriculumUnit = curriculumUnit;
-    }
+  // Método para editar una lección
+  editLesson(topic, description, curriculumUnit) {
+    this.topic = topic;
+    this.description = description;
+    this.curriculumUnit = curriculumUnit;
+  }
 
-    // Método para eliminar una lección
-    deleteLesson(lessonsArray) {
-        if (confirm("¿Estás seguro de que quieres eliminar este plan de clase?")) {
-            return lessonsArray.filter(lesson => lesson.id !== this.id);
-        }
-        // Retornar el mismo array si se cancela
-        return lessonsArray;
+  // Método para eliminar una lección
+  deleteLesson(lessonsArray) {
+    if (confirm("¿Estás seguro de que quieres eliminar este plan de clase?")) {
+      return lessonsArray.filter((lesson) => lesson.id !== this.id);
     }
+    // Retornar el mismo array si se cancela
+    return lessonsArray;
+  }
 }
