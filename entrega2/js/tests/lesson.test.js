379449104b2c7da5f04deeda3df023f5f3bb1407
ManuPerez <manuelperezmartirene@gmail.com>
2024-11-25 12:30:40 -0300

    Correcciones en los tests

--- entrega2/js/tests/lesson.test.js
+++ entrega2/js/tests/lesson.test.js
@@ -9,21 +9,26 @@ describe('Lesson', () => {
   beforeEach(() => {
     lesson = new Lesson(
       1,
-      "2024-11-11",
-      "Matemáticas",
-      "Descripción de la lección",
-      "Unidad 1",
-      "Juan Gomez"
+      '2024-11-11',
+      'Matemáticas',
+      'Descripción de la lección',
+      'Unidad 1',
+      'Juan Gomez'
     );
     lessonsArray = [lesson];
   });
 
-  test("debe editar correctamente los detalles de la lección", () => {
-    lesson.editLesson("Ciencias", "Nueva descripción", "Unidad 2", "Pedro Almodovar" );
-    expect(lesson.getTopic()).toBe("Ciencias");
-    expect(lesson.getDescription()).toBe("Nueva descripción");
-    expect(lesson.getCurriculumUnit()).toBe("Unidad 2");
-    expect(lesson.getStudentAsignado()).toBe("Pedro Almodovar");
+  test('debe editar correctamente los detalles de la lección', () => {
+    lesson.editLesson(
+      'Ciencias',
+      'Nueva descripción',
+      'Unidad 2',
+      'Pedro Almodovar'
+    );
+    expect(lesson.getTopic()).toBe('Ciencias');
+    expect(lesson.getDescription()).toBe('Nueva descripción');
+    expect(lesson.getCurriculumUnit()).toBe('Unidad 2');
+    expect(lesson.getStudentAsignado()).toBe('Pedro Almodovar');
   });
 
   test('debe eliminar correctamente una lección', () => {
@@ -31,24 +36,23 @@ describe('Lesson', () => {
     expect(updatedLessons).toHaveLength(0);
   });
 
-  test("Función getDate", () => {
-    expect(lesson.getDate()).toBe("2024-11-11");
+  test('Función getDate', () => {
+    expect(lesson.getDate()).toBe('2024-11-11');
   });
 
-  test("Función getTopic", () => {
-    expect(lesson.getTopic()).toBe("Matemáticas");
+  test('Función getTopic', () => {
+    expect(lesson.getTopic()).toBe('Matemáticas');
   });
 
-  test( "Función getDescription", () => {
-    expect(lesson.getDescription()).toBe("Descripción de la lección");
+  test('Función getDescription', () => {
+    expect(lesson.getDescription()).toBe('Descripción de la lección');
   });
 
-  test("Función getCurriculumUnit", () => {
-    expect(lesson.getCurriculumUnit()).toBe("Unidad 1");
+  test('Función getCurriculumUnit', () => {
+    expect(lesson.getCurriculumUnit()).toBe('Unidad 1');
   });
 
-  test("Función getStudentAsignado", () => {
-    expect(lesson.getStudentAsignado()).toBe(  "Juan Gomez");
+  test('Función getStudentAsignado', () => {
+    expect(lesson.getStudentAsignado()).toBe('Juan Gomez');
   });
-
 });
